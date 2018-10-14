a3c4ab8974351d9594cc4b0a77dbc3cfcbb903e9
An Ha <zero579911@gmail.com>
2018-10-13 23:15:21 -0400

    ui: allow Tiles to be draggable and update view on remove for HandView

--- src/main/java/controller/HandController.java
+++ src/main/java/controller/HandController.java
@@ -1,21 +1,27 @@
 package controller;
 
 import factory.TileButtonFactory;
+import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import model.Game;
 import model.Hand;
 import model.Tile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ui.TileButton;
+import ui.UIHelper;
 
 import java.text.MessageFormat;
 
 public class HandController {
 	private static final Logger logger = LogManager.getLogger(HandController.class.getName());
+
+	@FXML
+	private VBox root;
 
 	@FXML
 	private Label lblPlayerNumber;
@@ -26,30 +32,20 @@ public class HandController {
 	private final Game game;
 	private final Hand hand;
 	private final int playerNumber;
-	private TileButtonFactory tileButtonFactory;
+	private final TileButtonFactory tileButtonFactory;
 
 	public HandController(Game game, int playerNumber) {
 		this.game = game;
 		this.playerNumber = playerNumber;
+		this.tileButtonFactory = new TileButtonFactory();
 		switch (playerNumber) {
 			case 1:
 				this.hand = game.getPlayer1Hand();
 				break;
-			case 2:
-				this.hand = game.getPlayer2Hand();
-				break;
-			case 3:
-				this.hand = game.getPlayer3Hand();
-				break;
-			case 4:
-				this.hand = game.getPlayer4Hand();
-				break;
 			default:
 				this.hand = null;
 				logger.error("Invalid player number: " + playerNumber);
-				return;
 		}
-		tileButtonFactory = new TileButtonFactory();
 	}
 
 	@FXML
@@ -57,7 +53,7 @@ public class HandController {
 		logger.info("Initializing Hand for Player " + playerNumber);
 		switch (playerNumber) {
 			case 1:
-				lblPlayerNumber.setText("Player 1");
+				Platform.runLater(() -> lblPlayerNumber.setText("Player 1"));
 				break;
 			default:
 				logger.error("Can't create a HandController for Player " + playerNumber);
@@ -66,17 +62,23 @@ public class HandController {
 		hand.getTiles().addListener(onTileListChange());
 	}
 
-	public ListChangeListener<Tile> onTileListChange() {
+	private ListChangeListener<Tile> onTileListChange() {
 		return (ListChangeListener.Change<? extends Tile> change) -> {
 			while (change.next()) {
 				if (change.wasAdded()) {
 					logger.debug(MessageFormat.format("Adding {0} to Player {1}s hand", change.getAddedSubList().toString(), playerNumber));
 					for (Tile t : change.getAddedSubList()) {
-						var btn = tileButtonFactory.newTileButton(t.getRank(), t.getColour(), false);
-						fpHand.getChildren().add(btn);
+						var btn = UIHelper.makeDraggable(tileButtonFactory.newTileButton(t, false), root);
+						Platform.runLater(() -> fpHand.getChildren().add(btn));
 					}
 				} else if (change.wasRemoved()) {
 					logger.debug(MessageFormat.format("Removing {0} in Player {1}s hand", change.getRemoved().toString(), playerNumber));
+					change.getRemoved().forEach(tile -> {
+						fpHand.getChildren().stream()
+								.filter(b -> ((TileButton) b).getTile().equals(tile))
+								.findAny()
+								.ifPresent(btn -> Platform.runLater(() -> fpHand.getChildren().remove(btn)));
+					});
 				}
 			}
 		};
