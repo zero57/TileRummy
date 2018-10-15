eff06c3291fe4662e90fee65086ac275886c5c88
ABRAR-COMPUTER\Abrar <abrarkazi@hotmail.com>
2018-10-14 20:53:35 -0400

    test: add tests for meld validation

--- src/test/java/model/MeldTest.java
+++ src/test/java/model/MeldTest.java
@@ -1,16 +1,16 @@
 package model;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.empty;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class MeldTest {
 	private Meld meld;
@@ -19,67 +19,199 @@ public class MeldTest {
 	public void setUp() {
 		meld = new Meld();
 	}
-	
+
 	@Test
-	public void testMeldAddFirst() {
-		Tile t1 = new Tile(5,Tile.Colours.RED);
-		Tile t2 = new Tile(4,Tile.Colours.RED);
-		Tile t3 = new Tile(3,Tile.Colours.RED);
+	public void testRunAddFirst() {
+		Tile t1 = new Tile(5, Tile.Colours.RED);
+		Tile t2 = new Tile(4, Tile.Colours.RED);
+		Tile t3 = new Tile(3, Tile.Colours.RED);
 		meld.addFirstTile(t1);
 		meld.addFirstTile(t2);
 		meld.addFirstTile(t3);
-		assertThat(meld.getMeld(),contains(t3,t2,t1));//last tile added should be first tile in meld
+		assertThat(meld.getMeld(), contains(t3, t2, t1));//last tile added should be first tile in meld
+		//Now attempting to add invalid tiles
+		meld.addFirstTile(new Tile(2, Tile.Colours.BLUE));
+		meld.addFirstTile(new Tile(3, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(1, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(7, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(3, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(4, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(5, Tile.Colours.GREEN));
+		assertThat(meld.getMeld(), contains(t3, t2, t1));
 	}
-	
+
 	@Test
-	public void testMeldAddLast() {
-		Tile t1 = new Tile(10,Tile.Colours.RED);
-		Tile t2 = new Tile(10,Tile.Colours.GREEN);
-		Tile t3 = new Tile(10,Tile.Colours.ORANGE);
+	public void testRunAddLast() {
+		Tile t1 = new Tile(3, Tile.Colours.RED);
+		Tile t2 = new Tile(4, Tile.Colours.RED);
+		Tile t3 = new Tile(5, Tile.Colours.RED);
 		meld.addLastTile(t1);
 		meld.addLastTile(t2);
 		meld.addLastTile(t3);
-		assertThat(meld.getMeld(),contains(t1,t2,t3));
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
+		//Now attempting to add invalid tiles
+		meld.addFirstTile(new Tile(6, Tile.Colours.BLUE));
+		meld.addFirstTile(new Tile(5, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(7, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(1, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(3, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(4, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(5, Tile.Colours.GREEN));
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
 	}
-	
+
 	@Test
-	public void testMeldAddArbitrary() {
-		Tile t1 = new Tile(1,Tile.Colours.BLUE);
-		Tile t2 = new Tile(2,Tile.Colours.BLUE);
-		Tile t3 = new Tile(3,Tile.Colours.BLUE);
-		Tile t4 = new Tile(4,Tile.Colours.BLUE);
-		Tile t5 = new Tile(5,Tile.Colours.BLUE);
+	public void testSetAddFirst() {
+		Tile t1 = new Tile(10, Tile.Colours.RED);
+		Tile t2 = new Tile(10, Tile.Colours.GREEN);
+		Tile t3 = new Tile(10, Tile.Colours.ORANGE);
 		meld.addFirstTile(t1);
 		meld.addFirstTile(t2);
-		meld.addLastTile(t3);
-		meld.addLastTile(t4);
-		meld.addFirstTile(t5);
-		assertThat(meld.getMeld(),contains(t5,t2,t1,t3,t4));
+		meld.addFirstTile(t3);
+		assertThat(meld.getMeld(), contains(t3, t2, t1));
+		//Now attempting to add invalid tiles
+		meld.addFirstTile(new Tile(6, Tile.Colours.BLUE));
+		meld.addFirstTile(new Tile(13, Tile.Colours.BLUE));
+		meld.addFirstTile(new Tile(10, Tile.Colours.RED));
+		meld.addFirstTile(new Tile(10, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(9, Tile.Colours.RED));
+		assertThat(meld.getMeld(), contains(t3, t2, t1));
 	}
-	
+
 	@Test
-	public void testMeldRemove() {
-		Tile t1 = new Tile(1,Tile.Colours.RED);
-		Tile t2 = new Tile(2,Tile.Colours.GREEN);
-		Tile t3 = new Tile(3,Tile.Colours.ORANGE);
-		Tile t4 = new Tile(4,Tile.Colours.BLUE);
-		Tile t5 = new Tile(5,Tile.Colours.BLUE);
+	public void testSetAddLast() {
+		Tile t1 = new Tile(10, Tile.Colours.RED);
+		Tile t2 = new Tile(10, Tile.Colours.GREEN);
+		Tile t3 = new Tile(10, Tile.Colours.ORANGE);
 		meld.addLastTile(t1);
 		meld.addLastTile(t2);
 		meld.addLastTile(t3);
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
+		//Now attempting to add invalid tiles
+		meld.addLastTile(new Tile(6, Tile.Colours.BLUE));
+		meld.addLastTile(new Tile(13, Tile.Colours.BLUE));
+		meld.addLastTile(new Tile(10, Tile.Colours.RED));
+		meld.addLastTile(new Tile(10, Tile.Colours.GREEN));
+		meld.addLastTile(new Tile(11, Tile.Colours.ORANGE));
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
+	}
+
+	@Test
+	public void testMeldRemove() {
+		Tile t1 = new Tile(4, Tile.Colours.RED);
+		Tile t2 = new Tile(3, Tile.Colours.RED);
+		Tile t3 = new Tile(5, Tile.Colours.RED);
+		Tile t4 = new Tile(6, Tile.Colours.RED);
+		Tile t5 = new Tile(7, Tile.Colours.RED);
+		meld.addLastTile(t1);
+		meld.addFirstTile(t2);
+		meld.addLastTile(t3);
+		meld.removeFirstTile();
 		meld.addLastTile(t4);
 		meld.addLastTile(t5);
-		
-		meld.removeTile(t4);
-		assertThat(meld.getMeld(),contains(t1,t2,t3,t5));
-		meld.removeTile(t1);
-		assertThat(meld.getMeld(),contains(t2,t3,t5));
-		meld.removeTile(t3);
-		assertThat(meld.getMeld(),contains(t2,t5));
-		meld.removeTile(t5);
-		assertThat(meld.getMeld(),contains(t2));
-		meld.removeTile(t2);
-		assertThat(meld.getMeld(),is(empty()));
+		meld.removeLastTile();
+		assertThat(meld.getMeld(), contains(t1, t3, t4));
+		meld.removeLastTile();
+		meld.removeLastTile();
+		meld.removeLastTile();
+		assertThat(meld.getMeld(), is(empty()));
+		meld.removeLastTile();
+		meld.removeFirstTile();
+		assertThat(meld.getMeld(), is(empty()));
 	}
-	
+
+	@Test
+	public void testRunToSet() {
+		//create a run
+		Tile t1 = new Tile(3, Tile.Colours.RED);
+		meld.addLastTile(t1);
+		meld.addLastTile(new Tile(4, Tile.Colours.RED));
+		meld.addLastTile(new Tile(5, Tile.Colours.RED));
+		//remove all cards but one
+		meld.removeLastTile();
+		meld.removeLastTile();
+		//create a set
+		Tile t2 = new Tile(3, Tile.Colours.ORANGE);
+		Tile t3 = new Tile(3, Tile.Colours.GREEN);
+		meld.addLastTile(t2);
+		meld.addLastTile(t3);
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
+		//shouldn't behave as a run
+		meld.addFirstTile(new Tile(2, Tile.Colours.RED));
+		meld.addLastTile(new Tile(4, Tile.Colours.GREEN));
+		assertThat(meld.getMeld(), contains(t1, t2, t3));
+	}
+
+	@Test
+	public void testSetToRun() {
+		//create a set
+		Tile t1 = new Tile(3, Tile.Colours.RED);
+		meld.addLastTile(t1);
+		meld.addLastTile(new Tile(3, Tile.Colours.ORANGE));
+		meld.addLastTile(new Tile(3, Tile.Colours.GREEN));
+		//remove all cards but one
+		meld.removeLastTile();
+		meld.removeLastTile();
+		//create a run
+		Tile t2 = new Tile(2, Tile.Colours.RED);
+		Tile t3 = new Tile(4, Tile.Colours.RED);
+		meld.addFirstTile(t2);
+		meld.addLastTile(t3);
+		assertThat(meld.getMeld(), contains(t2, t1, t3));
+		//shouldn't behave as a set
+		meld.addFirstTile(new Tile(2, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(3, Tile.Colours.GREEN));
+		meld.addFirstTile(new Tile(4, Tile.Colours.GREEN));
+		meld.addLastTile(new Tile(2, Tile.Colours.GREEN));
+		meld.addLastTile(new Tile(3, Tile.Colours.GREEN));
+		meld.addLastTile(new Tile(4, Tile.Colours.GREEN));
+		assertThat(meld.getMeld(), contains(t2, t1, t3));
+	}
+
+	@Test
+	public void testValue() {
+		assertEquals(0, meld.getValue());
+		meld.addLastTile(new Tile(4, Tile.Colours.RED));
+		assertEquals(4, meld.getValue());
+		meld.addLastTile(new Tile(5, Tile.Colours.RED));
+		assertEquals(9, meld.getValue());
+		meld.addFirstTile(new Tile(3, Tile.Colours.RED));
+		assertEquals(12, meld.getValue());
+		meld.addLastTile(new Tile(7, Tile.Colours.GREEN));
+		assertEquals(12, meld.getValue());
+		meld.addFirstTile(new Tile(1, Tile.Colours.ORANGE));
+		assertEquals(12, meld.getValue());
+		meld.removeLastTile();
+		assertEquals(7, meld.getValue());
+		meld.removeFirstTile();
+		assertEquals(4, meld.getValue());
+		meld.removeLastTile();
+		assertEquals(0, meld.getValue());
+		meld.removeLastTile();
+		assertEquals(0, meld.getValue());
+	}
+
+	@Test
+	public void testValid() {
+		assertFalse(meld.isValid());
+		//now add cards
+		meld.addLastTile(new Tile(4, Tile.Colours.RED));
+		assertFalse(meld.isValid());
+		meld.addLastTile(new Tile(5, Tile.Colours.RED));
+		assertFalse(meld.isValid());
+		meld.addFirstTile(new Tile(2, Tile.Colours.RED));
+		assertFalse(meld.isValid());
+		meld.addLastTile(new Tile(2, Tile.Colours.RED));
+		assertFalse(meld.isValid());
+		meld.addFirstTile(new Tile(3, Tile.Colours.RED));
+		assertTrue(meld.isValid());
+		meld.addFirstTile(new Tile(2, Tile.Colours.RED));
+		assertTrue(meld.isValid());
+		meld.removeLastTile();
+		assertTrue(meld.isValid());
+		meld.removeFirstTile();
+		assertFalse(meld.isValid());
+		meld.removeLastTile();
+		assertFalse(meld.isValid());
+	}
 }
