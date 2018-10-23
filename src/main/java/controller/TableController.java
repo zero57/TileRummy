ebfe3555edfab273cba937c1e352876a74b9d51a
An Ha <zero579911@gmail.com>
2018-10-23 17:36:16 -0400

    ui: allow user to DragNDrop from hand to table and table to table

--- src/main/java/controller/TableController.java
+++ src/main/java/controller/TableController.java
@@ -1,14 +1,24 @@
 package controller;
 
 import factory.TileButtonFactory;
+import javafx.application.Platform;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import model.Game;
 import model.Tile;
+import model.observable.ObservableMeld;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ui.TileButton;
+import ui.UIHelper;
+
+import java.text.MessageFormat;
+import java.util.Optional;
 
 public class TableController {
 
@@ -19,10 +29,12 @@ public class TableController {
 
 	private final Game game;
 	private final TileButtonFactory tileButtonFactory;
+	private final ObservableList<ObservableMeld> table;
 
 	public TableController(Game game) {
 		this.game = game;
 		this.tileButtonFactory = new TileButtonFactory();
+		this.table = game.getTable();
 	}
 
 	@FXML
@@ -37,13 +49,94 @@ public class TableController {
 				logger.debug("On MouseDragRelease " + row + " " + col);
 				TileButton btn = (TileButton) e.getGestureSource();
 				Tile tile = btn.getTile();
-				game.getPlayer1Hand().removeTile(tile);
-				// todo: add logic to adding tile to Game table
-				// this code below is for temporary testing purposes
-				// gpTable.add(tileButtonFactory.newTileButton(tile), col, row);
+				// Click and drag tile from table to table
+				if (btn.getOriginatesFromTable()) {
+					if (game.removeTileFromTable(tile, btn.getRow(), btn.getCol())) {
+						if (!game.addTileToTable(tile, row, col)) {
+							// add it back if you can't add it to the spot
+							game.addTileToTable(tile, btn.getRow(), btn.getCol());
+						}
+					}
+				} else {
+					// Click and drag tile from hand to table
+					if (game.addTileToTable(tile, row, col)) {
+						game.getPlayer1Hand().removeTile(tile);
+					}
+				}
 			});
+		});
+		table.addListener(onTableMeldListChange());
+	}
 
+	private ListChangeListener<ObservableMeld> onTableMeldListChange() {
+		return (ListChangeListener.Change<? extends ObservableMeld> change) -> {
+			while (change.next()) {
+				if (change.wasAdded()) {
+					change.getAddedSubList().forEach(meld -> {
+						logger.debug(MessageFormat.format("Adding meld {0} to table at row:{1} col:{2} with length: {3}", meld, meld.getRow() + 1, meld.getCol() + 1, meld.getSize()));
+						updateTable();
+						meld.getMeld().addListener((ListChangeListener<Tile>) change1 -> {
+							while (change1.next()) {
+								if (change1.wasAdded()) {
+									logger.debug(MessageFormat.format("Adding {0} to meld {1}", change1.getAddedSubList().toString(), meld.toString()));
+								} else if (change1.wasRemoved()) {
+									logger.debug(MessageFormat.format("Removing {0} in meld {1}", change1.getRemoved().toString(), meld.toString()));
+								}
+							}
+							updateTable();
+						});
+					});
+				}
+			}
+		};
+	}
+
+	private void updateTable() {
+		Platform.runLater(() -> {
+			clearTable();
+			table.forEach(this::addMeldToTable);
 		});
 	}
 
+	private void clearTable() {
+		// Note, when calling this function, you should wrap it with Platform.runLater. It is not done here as having
+		// too many runnables can cause issues such as flickering
+		for (int row = 0; row < gpTable.getRowCount(); row++) {
+			for (int col = 0; col < gpTable.getColumnCount(); col++) {
+				getCellFromGridPane(row, col).ifPresent(hbox -> hbox.getChildren().clear());
+			}
+		}
+	}
+
+	private void addMeldToTable(ObservableMeld meld) {
+		// Note, when calling this function, you should wrap it with Platform.runLater. It is not done here as having
+		// too many runnables can cause issues such as flickering
+		for (int i = 0; i < meld.getSize(); i++) {
+			// Ignore the cell containing the tile
+			var ignoreRootNode = getCellFromGridPane(meld.getRow(), meld.getCol() + i).orElseThrow(); // We throw, but this should never happen as there will always be an HBox in the GridCell
+			var btn = UIHelper.makeDraggable(tileButtonFactory.newTileButton(meld.getMeld().get(i), false, meld.getRow(), meld.getCol() + i), ignoreRootNode);
+			addOrReplaceNodeAtCell(btn, meld.getRow(), meld.getCol() + i);
+		}
+	}
+
+	private void addOrReplaceNodeAtCell(Node node, int row, int col) {
+		// Note, when calling this function, you should wrap it with Platform.runLater. It is not done here as having
+		// too many runnables can cause issues such as flickering
+		getCellFromGridPane(row, col).ifPresent(cell -> {
+			if (cell.getChildren().size() > 0) {
+				cell.getChildren().setAll(node);
+			} else {
+				cell.getChildren().add(node);
+			}
+		});
+	}
+
+	private Optional<HBox> getCellFromGridPane(int row, int col) {
+		for (Node node : gpTable.getChildren()) {
+			if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col) {
+				return Optional.of((HBox) node);
+			}
+		}
+		return Optional.empty();
+	}
 }
