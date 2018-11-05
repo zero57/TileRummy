9d218b4713c66a3b6282aff5db370bd791727db5
An Ha <zero579911@gmail.com>
2018-11-05 05:26:53 -0500

    backend: add listeners for player who wins

--- src/main/java/model/Hand.java
+++ src/main/java/model/Hand.java
@@ -1,6 +1,9 @@
 package model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 import model.observable.ObservableTile;
@@ -11,15 +14,20 @@ public class Hand {
 
 	private ObservableList<ObservableTile> unsortedTiles;
 	private ObservableList<ObservableTile> tiles;
+	private IntegerProperty sizeProperty;
 
 	public Hand() {
 		unsortedTiles = FXCollections.observableArrayList();
 		tiles = new SortedList<>(unsortedTiles, Comparator.naturalOrder());
+		sizeProperty = new SimpleIntegerProperty(0);
+		unsortedTiles.addListener((ListChangeListener<ObservableTile>) change -> sizeProperty.set(unsortedTiles.size()));
 	}
 
 	public Hand(Hand hand) {
 		unsortedTiles = FXCollections.observableArrayList(hand.getTiles());
 		tiles = new SortedList<>(unsortedTiles, Comparator.naturalOrder());
+		sizeProperty = new SimpleIntegerProperty(0);
+		unsortedTiles.addListener((ListChangeListener<ObservableTile>) change -> sizeProperty.set(unsortedTiles.size()));
 	}
 
 	public boolean addTile(ObservableTile tile) {
@@ -36,6 +44,10 @@ public class Hand {
 
 	public ObservableList<ObservableTile> getUnsortedTiles() {
 		return unsortedTiles;
+	}
+
+	public IntegerProperty getSizeProperty() {
+		return sizeProperty;
 	}
 
 	public void clear() {
